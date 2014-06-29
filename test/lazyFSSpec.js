5c80499fd05e772b787a8d90878fe22f55300657
Eugene Krevenets <hyzhak@ya.ru>
2014-06-29 20:30:43 +0200

    return stat are result

--- test/lazyFSSpec.js
+++ test/lazyFSSpec.js
@@ -24,16 +24,37 @@ describe('lazy-fs', function() {
     it('should iterate thought files and directives in folder', function(done) {
         try {
             lazyfs.dir('test-folder', { recursive: true })
+//                current implementation of lazy.js doesn't have sortBy for AsyncSequence
+//                .sortBy(lazyfs.nameOfFile)
                 .toArray()
                 .then(function(files) {
                     try {
                         expect(files).to.have.length(6);
-                        expect(files).to.contain(path.join('test-folder', 'path-to-fake-dir'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-some.png'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-some.txt'));
-                        expect(files).to.contain(path.join('test-folder', 'some-other-path'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-fake-dir', 'some-file.txt'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-fake-dir', 'empty-dir'));
+
+                        expect(files[0]).to.have.property('name', 'some-other-path');
+                        expect(files[0]).to.have.property('root', 'test-folder');
+                        expect(files[0]).to.have.property('path', path.join('test-folder', 'some-other-path'));
+
+                        expect(files[1]).to.have.property('name', 'path-to-some.txt');
+                        expect(files[1]).to.have.property('root', 'test-folder');
+                        expect(files[1]).to.have.property('path', path.join('test-folder', 'path-to-some.txt'));
+
+                        expect(files[2]).to.have.property('name', 'path-to-some.png');
+                        expect(files[2]).to.have.property('root', 'test-folder');
+                        expect(files[2]).to.have.property('path', path.join('test-folder', 'path-to-some.png'));
+
+                        expect(files[3]).to.have.property('name', 'path-to-fake-dir');
+                        expect(files[3]).to.have.property('root', 'test-folder');
+                        expect(files[3]).to.have.property('path', path.join('test-folder', 'path-to-fake-dir'));
+
+                        expect(files[4]).to.have.property('name', 'some-file.txt');
+                        expect(files[4]).to.have.property('root', path.join('test-folder', 'path-to-fake-dir'));
+                        expect(files[4]).to.have.property('path', path.join('test-folder', 'path-to-fake-dir', 'some-file.txt'));
+
+                        expect(files[5]).to.have.property('name', 'empty-dir');
+                        expect(files[5]).to.have.property('root', path.join('test-folder', 'path-to-fake-dir'));
+                        expect(files[5]).to.have.property('path', path.join('test-folder', 'path-to-fake-dir', 'empty-dir'));
+
                         done();
                     } catch(e) {
                         done(e);
@@ -53,10 +74,22 @@ describe('lazy-fs', function() {
                 .then(function(files) {
                     try {
                         expect(files).to.have.length(4);
-                        expect(files).to.contain(path.join('test-folder', 'path-to-fake-dir'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-some.png'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-some.txt'));
-                        expect(files).to.contain(path.join('test-folder', 'some-other-path'));
+
+                        expect(files[0]).to.have.property('name', 'some-other-path');
+                        expect(files[0]).to.have.property('root', 'test-folder');
+                        expect(files[0]).to.have.property('path', path.join('test-folder', 'some-other-path'));
+
+                        expect(files[1]).to.have.property('name', 'path-to-some.txt');
+                        expect(files[1]).to.have.property('root', 'test-folder');
+                        expect(files[1]).to.have.property('path', path.join('test-folder', 'path-to-some.txt'));
+
+                        expect(files[2]).to.have.property('name', 'path-to-some.png');
+                        expect(files[2]).to.have.property('root', 'test-folder');
+                        expect(files[2]).to.have.property('path', path.join('test-folder', 'path-to-some.png'));
+
+                        expect(files[3]).to.have.property('name', 'path-to-fake-dir');
+                        expect(files[3]).to.have.property('root', 'test-folder');
+                        expect(files[3]).to.have.property('path', path.join('test-folder', 'path-to-fake-dir'));
                         done();
                     } catch(e) {
                         done(e);
@@ -72,17 +105,21 @@ describe('lazy-fs', function() {
     it('should filter files', function(done) {
         try {
             lazyfs.dir('test-folder', { recursive: false })
-                .filter(lazyfs.fileOnly)
+                .filter(lazyfs.isFile)
                 .async()
                 .toArray()
                 .then(function(files) {
                     try {
-                        expect(files).not.to.be.null;
-                        expect(files).not.to.be.undefined;
-
                         expect(files).to.have.length(2);
-                        expect(files).to.contain(path.join('test-folder', 'path-to-some.png'));
-                        expect(files).to.contain(path.join('test-folder', 'path-to-some.txt'));
+
+                        expect(files[0]).to.have.property('name', 'path-to-some.txt');
+                        expect(files[0]).to.have.property('root', 'test-folder');
+                        expect(files[0]).to.have.property('path', path.join('test-folder', 'path-to-some.txt'));
+
+                        expect(files[1]).to.have.property('name', 'path-to-some.png');
+                        expect(files[1]).to.have.property('root', 'test-folder');
+                        expect(files[1]).to.have.property('path', path.join('test-folder', 'path-to-some.png'));
+
                         done();
                     } catch(e) {
                         done(e);
